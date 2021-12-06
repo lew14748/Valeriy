96769ce0ee76f8219a410d82f3d172ddb6d64e4c
NikitaIVT <nikita-polishhuk@mail.ru>
2021-12-06 19:20:20 +0700

    commiting some progress at load system

--- lib/states/load.rb
+++ lib/states/load.rb
@@ -1,13 +1,35 @@
 module AppStates
     class Load < BaseState
         def run
-            render
-            #save
+            find_save_folder
             context.transition_to_state AppStates::Welcome.new
         end
 
-        def render
-            io_adapter.write 'you are in loading state'
+        def find_save_folder
+            if File.directory?("saves")
+                find_saves
+            else
+                Dir.mkdir("saves")
+            end
+        end
+
+        def find_saves
+            @saves = Dir.glob("saves/*.yml")
+            if @saves.any?
+                io_adapter.write 'Choose your save'
+                show_saves @saves
+            else
+                io_adapter.write 'You have not any saves'
+                context.transition_to_state AppStates::Welcome.new
+            end
+        end
+
+        def show_saves saves
+            num = 0
+            for save in saves do 
+                num = num + 1
+                io_adapter.write "[#{num}]#{save}"
+            end
         end
     end
 end
