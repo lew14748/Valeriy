bee9c9802763edae07499fb01a656df671a28de8
eduardshindel <shindeleduard10@gmail.com>
2021-12-13 18:27:34 +0700

    rubocop fix action_spec.rb

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe Action do
       before { actions.do_effect }
       it 'money increased' do expect(valera.money).to eq 100 end
       it 'fatigue increased' do expect(valera.fatigue).to eq 20 end
-      it 'fun decreased' do expect(valera.fun).to eq -1 end
+      it 'fun decreased' do expect(valera.fun).to eq(-1) end
       it 'health decreased' do expect(valera.health).to eq 90 end
       it 'mana has not changed' do expect(valera.mana).to eq 0 end
     end
@@ -64,7 +64,7 @@ RSpec.describe Action do
         before { actions.do_action }
         it 'money increased' do expect(valera.money).to eq 100 end
         it 'fatigue increased' do expect(valera.fatigue).to eq 20 end
-        it 'fun decreased' do expect(valera.fun).to eq -1 end
+        it 'fun decreased' do expect(valera.fun).to eq(-1) end
         it 'health decreased' do expect(valera.health).to eq 90 end
         it 'mana has not changed' do expect(valera.mana).to eq 0 end
       end
